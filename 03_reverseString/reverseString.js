6212d8a6223475a4c5ff27a042f026f0cb0d53af
Michael Wheatfill <michael@wheatfill.com>
2022-03-06 10:30:02 -0700

    Completed Exercises 1 - 5

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  const stringArray = string.split('');
+  let reverseString = '';
 
+  for (let i = stringArray.length -1; i >= 0; --i) {
+    reverseString += stringArray[i];
+  }
+  return reverseString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...itemsToRemove) {
+  const newArray = [];
 
+  array.forEach((item) => {
+    if (!itemsToRemove.includes(item)) {
+      newArray.push(item);
+    }
+  });
+
+  return newArray;
 };
 
 // Do not edit below this line
