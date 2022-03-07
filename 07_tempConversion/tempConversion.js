c16027a50f0606dc269547c2f6f8685c77d9532e
Michael Wheatfill <michael@wheatfill.com>
2022-03-07 13:44:21 -0700

    Completed Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  //formula is (x - 32) * .5556
+  let c = (temp - 32) * .5556
+  return Number(Math.round(c + "e+1") + "e-1")
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  //formula is (x * 1.8) + 32
+  let f = (temp * 1.8) + 32
+  return Number(Math.round(f + "e+1") + "e-1")
 };
 
 // Do not edit below this line
