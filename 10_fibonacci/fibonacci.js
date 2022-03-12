2d491a3847f6f156c6f692ae7025c3681b3d8f4d
Michael Wheatfill <michael@wheatfill.com>
2022-03-12 13:31:32 -0700

    Completed fibonacci challenge and passed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n <= 0) {
+    return 'OOPS';
+  }
 
+  let previousN = 1;
+  let nextN = 1;
+  let array = [previousN, nextN];
+
+  for (i = 1; i < n; i++) {
+    let sum = previousN + nextN;
+    array.push(sum);
+    previousN = nextN;
+    nextN = sum;
+  }
+  return array[n - 1];
 };
 
 // Do not edit below this line
