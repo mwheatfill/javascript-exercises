379e6e800a390d067c05080507591def19da5402
Michael Wheatfill <michael@wheatfill.com>
2022-03-12 14:12:13 -0700

    Completed the exercise, wondering how I could refactor to be more efficient

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const oldest = people.sort((a, b) => {
+    if (typeof(a.yearOfDeath) !== 'number') {a.yearOfDeath = new Date().getFullYear()}
+    if (typeof(b.yearOfDeath) !== 'number') {b.yearOfDeath = new Date().getFullYear()}
+    return a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? -1 : 1
+  })
+  return oldest[0];
 };
 
 // Do not edit below this line
