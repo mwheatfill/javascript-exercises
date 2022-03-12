7ec4046abe497a6f148f62597eb7ca0e5398ec55
Michael Wheatfill <michael@wheatfill.com>
2022-03-11 20:04:01 -0700

    Completed calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  return numbers.reduce((sum, number) => sum + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.length
+    ? numbers.reduce((product, number) => product * number)
+    : 0;
 };
 
-const power = function() {
-	
+const power = function (num, exp) {
+  return num ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let result = 1;
+  for (let i = 1; i < n; i++) {
+    result *= i + 1
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +35,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
