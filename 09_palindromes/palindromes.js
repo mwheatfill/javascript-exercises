9643c1f364dcd79c6e027acf5e3166483155ab4c
Michael Wheatfill <michael@wheatfill.com>
2022-03-11 20:59:14 -0700

    Completed palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  let array = text.toLowerCase().match(/[a-zA-Z]/g);
+  let forwardText = array.join('');
+  let reverseText = '';
 
+  for (i = array.length - 1; i >= 0; i--) {
+    reverseText += array[i];
+  }
+  return forwardText === reverseText;
 };
 
 // Do not edit below this line
